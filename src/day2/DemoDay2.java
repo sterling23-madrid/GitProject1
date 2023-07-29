dd893cbe6e3599852ade9b11371ff23274842bc2
sterling23-madrid <120076006+sterling23-madrid@users.noreply.github.com>
2023-07-29 10:52:36 -0600

    added new class

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -3,6 +3,7 @@ package day2;
 public class DemoDay2 {
 
 	public static void main(String[] args) {
+		System.out.println("sterling");
 		System.out.println("Test");
 
 	}
---
+++ src/day5/DemoDay5.java
@@ -0,0 +1,8 @@
+package day5;
+
+public class DemoDay5 {
+	public static void main(String[] args) {
+		System.out.println("Today is sunny......");
+	}
+
+}
