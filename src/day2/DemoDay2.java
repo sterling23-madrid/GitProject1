e8f744940f68ede205dfa5b6d13b62a147cffdd1
sterling23-madrid <120076006+sterling23-madrid@users.noreply.github.com>
2023-07-29 09:16:53 -0600

    day3

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -1,38 +1,31 @@
 package day2;
 
 public class DemoDay2 {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Test");
-		
-		
-	}
-	
-	// add method 
-	public static void add (int a, int b) {
-		System.out.println(a+b);
-		
-	}
-	
-	// subtract method 
-	public static void subtract (int a, int b){
-		System.out.println(a-b);
-	}
-
-	//Yasir
-	public static void divide (int a, int b) {
-		System.out.println(a/b);
-
 
 	}
-	// add method 
-	public static void add (int a, int b, int c) {
-		System.out.println(a+b +c);	
+
+	// add method
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 
 	}
-	
+
+	// subtract method
+	public static void subtract(int a, int b) {
+		System.out.println(a - b);
+	}
+
+// add method 
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+
+	}
+
 //add method
-	public static void divide (double a, double b) {
-		System.out.println(a+b);
+	public static void divide(double a, double b) {
+		System.out.println(a + b);
 	}
 }
