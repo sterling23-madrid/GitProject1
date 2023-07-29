5cdaf5984727426f855cfae48d48cf40ccd42d88
sterling23-madrid <120076006+sterling23-madrid@users.noreply.github.com>
2023-07-29 11:21:17 -0600

    new method

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -4,6 +4,10 @@ public class DemoDay5 {
 	public static void main(String[] args) {
 		System.out.println("Today is sunny......");
 	}
+	
+	public static void getInfo () {
+		System.out.println("My name is Sterling");
+	}
 
 	public static void getData (){
 		System.out.println("today");
